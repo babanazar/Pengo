2045e7e7530b36d3fd79c6fad435e37d3f903265
edabicakci <d.edabicakci@gmail.com>
2019-12-21 17:13:25 +0300

    iconlar

--- app/src/main/java/com/example/pengout/view/fragment/InfoFragment3.java
+++ app/src/main/java/com/example/pengout/view/fragment/InfoFragment3.java
@@ -20,23 +20,67 @@ import com.example.pengout.view.activity.HomeActivity;
 
 public class InfoFragment3 extends Fragment {
 
-    Button finish;
-    Button art;
+    Button ready;
+    Button art,business, education, entertainment,health,sport, trip, workshop ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_info3,container,false);
-        finish = view.findViewById(R.id.finishButton);
-        art = view.findViewById(R.id.imgButton);
+        ready = view.findViewById(R.id.button_ready);
+        art = view.findViewById(R.id.art2);
 
         art.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                art.getBackground().setAlpha(64);
             }
         });
-        finish.setOnClickListener(new View.OnClickListener() {
+        business.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                business.getBackground().setAlpha(64);
+
+            }
+        });
+        education.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                education.getBackground().setAlpha(64);
+            }
+        });
+        entertainment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                entertainment.getBackground().setAlpha(64);
+            }
+        });
+        health.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                health.getBackground().setAlpha(64);
+            }
+        });
+        sport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sport.getBackground().setAlpha(64);
+            }
+        });
+        trip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                trip.getBackground().setAlpha(64);
+            }
+        });
+        workshop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                workshop.getBackground().setAlpha(64);
+            }
+        });
+        ready.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), HomeActivity.class);
